28057635973b018b2a2cf9a3b0e9e57231ad4dcd
Nacho <icfuenza@gmail.com>
2019-12-24 19:38:19 -0300

    institution model methods

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,2 +1,42 @@
 class Institution < ApplicationRecord
+  attr_accessor :banko
+
+  def banko
+    @banko ||= Money::Bank::VariableExchange.new(self)
+  end
+
+  def rates
+    @rates ||= ExchangeRate.where(institution_id: self.id)
+  end
+
+  def set_bank
+    Money.default_bank = banko
+  end
+
+  def get_amount(from_iso_code, to_iso_code, from_amount)
+    set_bank
+    Money.from_amount(from_amount, from_iso_code).exchange_to(to_iso_code)
+  rescue Money::Bank::UnknownRate => e
+    nil
+  end
+
+  #
+  # get_rate is exposed to be accessible to Money::Bank::VariableExchange
+  # it's not supposed to be accessed directly.
+  #
+  def get_rate(from_iso_code, to_iso_code)
+    rate = rates.find_by(from: from_iso_code, to: to_iso_code)
+    rate.present? ? rate.rate : nil
+  end
+
+  #
+  # add_rate is exposed to be accessible to Money::Bank::VariableExchange
+  # it's not supposed to be accessed directly.
+  #
+  def add_rate(from_iso_code, to_iso_code, rate)
+    exrate = rates.find_or_initialize_by(from: from_iso_code, to: to_iso_code)
+    exrate.rate = rate
+    exrate.date = DateTime.now.utc
+    exrate.save!
+  end
 end
